c58e750c84fc77fb339b5bc1f763de760492c075
TonySilva7 <tony.deve7oper@gmail.com>
2023-10-16 22:17:55 -0300

    feat: configure react in package react

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@devlife-ui/tokens'
-
-console.log('react package' + colors.black)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@devlife-ui/tokens'
+
+export const App: React.FC = () => (
+  <div style={{ color: colors.devlife500 }}>Hello, world!</div>
+)
