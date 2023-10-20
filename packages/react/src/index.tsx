d4e77f79366169050e41e680816a72df3b3e8cea
TonySilva7 <tony.deve7oper@gmail.com>
2023-10-19 23:26:05 -0300

    feat: create pack 'docs'. Add and configure story book

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,9 +1,31 @@
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$devlife500',
-  borderRadius: '$md',
+  backgroundColor: '$devlife300',
+  borderRadius: '$sm',
+  border: 'none',
+  fontWeight: '$bold',
+  color: '$white',
+
+  variants: {
+    sizeVariant: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    sizeVariant: 'small',
+  },
 })
 
-export const App: React.FC = () => <Button>Hello, world!</Button>
+export type ButtonProps = {
+  sizeVariant: 'small' | 'big'
+}
